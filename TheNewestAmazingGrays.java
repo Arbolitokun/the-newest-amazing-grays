930e93084f6ceb07be9d99261b5b8dfefb0694cb
Arbolitokun <111543046+Arbolitokun@users.noreply.github.com>
2024-03-04 10:46:59 -0600

    Add files via upload

---
+++ TheNewestAmazingGrays.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package the.newest.amazing.grays;
+
+/**
+ *
+ * @author USER
+ */
+public class TheNewestAmazingGrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola papu");
+    }
+    
+}
